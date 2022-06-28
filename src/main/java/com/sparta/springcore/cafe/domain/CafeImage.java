bce0ddaefdb3e88cb073cb7753ea21d02d15bd84
taewan <ntw2100@gmail.com>
2022-06-28 09:50:07 +0900

    카페 관련 도메인 구현

--- src/main/java/com/sparta/springcore/cafe/domain/CafeImage.java
+++ src/main/java/com/sparta/springcore/cafe/domain/CafeImage.java
@@ -1,4 +1,28 @@
-package sparta.projectprac.cafe.domain;
+package com.sparta.springcore.cafe.domain;
 
-public class CafeImage {
+import com.sparta.springcore.model.Timestamped;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CafeImage extends Timestamped {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "cafe_image_number")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cafe_number")
+    private Cafe cafe;
+
+    private String cafeImageUrl;
 }
